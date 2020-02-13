d20b0349520184543a49b5867d14321d1c56b107
Simon Ismair <ismair.s@gmail.com>
2020-02-13 16:37:52 +0100

    Updated dependencies

--- dist/vDragDrop.js
+++ dist/vDragDrop.js
@@ -381,7 +381,7 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\OSS\v-drag-drop\src\index.js */"./src/index.js");
+module.exports = __webpack_require__(/*! D:\Simon\Code\_opensource\v-drag-drop\src\index.js */"./src/index.js");
 
 
 /***/ })
